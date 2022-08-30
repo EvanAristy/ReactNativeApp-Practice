93ac17fde5c6382659e1c6a96b7db8221fc18822
EvanAristy <evanjaristy@gmail.com>
2022-08-30 18:14:12 -0400

    Added another custom component for the input

--- App.js
+++ App.js
@@ -3,16 +3,12 @@ import { StyleSheet, Text, View, Button, TextInput, ScrollView, FlatList } from 
 
 // components
 import GoalItem from "./components/GoalItem";
+import GoalInput from "./components/GoalInput";
 
 export default function App() {
-  const [enteredGoalText, setEnteredGoalText] = useState('')
   const [courseGoals, setCourseGoals] = useState([])
 
-  function goalInputHandler(enteredText) {
-    setEnteredGoalText(enteredText)
-  }
-
-  function addGoalHandler() {
+  function addGoalHandler(enteredGoalText) {
     setCourseGoals(currentCourseGoals => [
       ...currentCourseGoals, 
       {text: enteredGoalText, key: Math.random().toString()}])
@@ -20,14 +16,7 @@ export default function App() {
 
   return (
     <View style={styles.appContainer} >
-      <View style={styles.inputContainer}>
-        <TextInput 
-          style={styles.textInput} 
-          placeholder="Your course goal!" 
-          onChangeText={goalInputHandler} 
-        />
-        <Button title="Add Goal" onPress={addGoalHandler} />
-      </View>
+      <GoalInput onAddGoal={addGoalHandler} />
       <View style={styles.goalsContainer}>
         <FlatList 
           data={courseGoals} 
@@ -49,24 +38,7 @@ const styles = StyleSheet.create({
     paddingTop: 50, 
     paddingHorizontal: 16
   },
-  inputContainer: {
-    flex: 1,
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    alignItems: 'center',
-    marginBottom: 24,
-    borderBottomWidth: 1,
-    borderBottomColor: '#cccccc'
-  },
-  textInput: {
-    borderWidth: 1,
-    borderColor: '#cccccc',
-    width: '70%',
-    marginRight: 8,
-    padding: 8
-  },
   goalsContainer: {
     flex: 5
   }, 
-  
 });
